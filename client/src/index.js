1826762a78eaa78f8e43668adbd7ce4d818cec6e
FK <f.kusztelak@gmail.com>
2019-05-24 22:02:03 +0200

    Redux form

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
@@ -9,7 +9,8 @@ import reducers from "./reducers";
 /**
  * @author Filip.Kusztelak
  */
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 /**
@@ -6,5 +7,6 @@ import authReducer from "./authReducer";
  */
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
